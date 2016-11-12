9db5935ec6c8143802a7b4fd3f83bbe23e825823
Tom-Kail <727266990@qq.com>
2016-11-12 11:43:08 +0800

    add demo

---
+++ demo/demo.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+	"os"
+
+	"github.com/Tom-Kail/email"
+)
+
+const (
+	Addr     = "smtp.xx.com"
+	Host     = Addr + ":25"
+	AuthName = "youmail@xx.com"
+	AuthPwd  = "youpasword"
+)
+
+func main() {
+	// gen attach
+	fileName := "文件"
+	f, err := os.Create(fileName)
+	defer os.Remove(f.Name())
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	f.WriteString("实践出真知")
+	m := email.NewMessage()
+	m.Attach(&fileName)
+	//	m.Attach(&fileName)
+	auth := smtp.PlainAuth(m.Sender, AuthName, AuthPwd, Addr)
+	// send email
+	count := 4
+	finish := make(chan bool)
+	//	mail, _ := os.Create("mail.txt")
+	//	mail.WriteString(msg)
+	for i := 0; i < count; i++ {
+		go func() {
+			defer func() { finish <- true }()
+			//send the email
+			if err := email.SendMail(&auth, m); err != nil {
+				fmt.Println(err)
+				return
+			}
+		}()
+	}
+
+	for i := 0; i < count; i++ {
+		<-finish
+	}
+}
